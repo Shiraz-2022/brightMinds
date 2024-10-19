349a6d9731766cf3f8b35059a13bf72be474f436
ShadowCoder151 <shadowcoder023@gmail.com>
2024-10-20 01:13:51 +0530

    updated handwriting_detection

--- ML/Models/handwriting_detection.py
+++ ML/Models/handwriting_detection.py
@@ -1,5 +1,6 @@
 import re
 import cv2
+import numpy as np
 from google.cloud import vision
 import io
 
@@ -9,13 +10,15 @@ def extract_handwriting(image_path):
         content = image_file.read()
 
     image = vision.Image(content=content)
-
     response = client.text_detection(image=image)
     texts = response.text_annotations
 
     if response.error.message:
         raise Exception(f'{response.error.message}')
     return texts[0].description if texts else "No handwriting detected"
+
+
+"""---------------------"""
 
 def preprocess_text(extracted_text):
     clean_text = extracted_text.strip()
@@ -27,10 +30,51 @@ def preprocess_text(extracted_text):
     clean_text = clean_text.replace("0", "O")  # Replace '0' with 'O'
     return clean_text
 
+
+"""---------------------"""
+
+def letter_check(input_text, expected_text):
+    return int(input_text == expected_text)
+
+"""---------------------"""
+
+def levenshtein_distance(word1, word2):
+    m, n = len(word1), len(word2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(m + 1):
+        dp[i][0] = i
+    for j in range(n + 1):
+        dp[0][j] = j
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if word1[i - 1] == word2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+    return int(dp[m][n])
+
+
+def word_check(input_text, expected_text):
+    dist = levenshtein_distance(input_text, expected_text)
+    return 1 - dist / max(len(input_text), len(expected_text))
+
+
+def sentence_check(input_text, expected_text):
+    pass
+
+
 def main(image_path):
+    letter = input('Enter the text:')
     extracted_text = extract_handwriting(image_path)
     final_text = preprocess_text(extracted_text)
+
+
     print("Extracted Handwriting Text:\n", final_text)
+    return final_text
+
+
+"""---------------------"""
 
 # Example usage
 if __name__ == "__main__":
